3d2875af7dc3185dea3b6abc03c7e91af69a0560
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-04 22:20:09 -0300

    [04/11/2022] Sequência de Pares.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Sequência de Pares.");
+Console.Write("Escolha um número de 0 à [?]: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (i <= max)
+{
+Console.Write($"{i} ");
+i = i + 2;
+}
+Console.WriteLine();
